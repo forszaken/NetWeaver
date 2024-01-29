89f4c67dbb5aa9abcf505ddf2487fca578b31c96
forszaken <snjtolibjonov@gmail.com>
2024-01-29 16:23:11 +0500

    Get lang from query params

--- public/index.php
+++ public/index.php
@@ -6,14 +6,25 @@ require __DIR__ . '/../vendor/autoload.php';
 
 http_response_code(500);
 
+function detectLang(string $default): string
+{
+    if (!empty($_GET['lang']) && is_string($_GET['lang'])) {
+        return $_GET['lang'];
+    }
+    return $default;
+}
+
 $name = $_GET['name'] ?? 'Guest';
 
 if (!is_string($name)) {
     http_response_code(400);
     exit;
 }
+
+$lang = detectLang('en');
+
 http_response_code(200);
 header('Content-Type: text/plain; charset=utf-8');
 header('X-Frame-Options: DENY');
 
-echo 'Hello, ' . $name . '!';
+echo 'Hello, ' . $name . '! Your lang is ' . $lang;

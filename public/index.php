249bfe8a3e0c7e4b6ad42d535844826a2cbf947f
forszaken <snjtolibjonov@gmail.com>
2024-01-29 16:20:06 +0500

    Fix http-status codes

--- public/index.php
+++ public/index.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+http_response_code(500);
+
 $name = $_GET['name'] ?? 'Guest';
 
 if (!is_string($name)) {
     http_response_code(400);
     exit;
 }
-
+http_response_code(200);
 header('Content-Type: text/plain; charset=utf-8');
 header('X-Frame-Options: DENY');
 

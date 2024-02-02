dcd23db44b0ff24ff23cdc837cb2cff6816d2d0b
forszaken <snjtolibjonov@gmail.com>
2024-02-02 05:35:20 +0500

    Update response test

--- tests/NetWeaver/Http/Message/ResponseTest.php
+++ tests/NetWeaver/Http/Message/ResponseTest.php
@@ -34,8 +34,9 @@ class ResponseTest extends TestCase
             []
         );
 
-        $response->setHeader('Header-1', 'value-1');
-        $response->setHeader('Header-2', 'value-2');
+        $response = $response
+            ->setHeader('Header-1', 'value-1')
+            ->setHeader('Header-2', 'value-2');
 
         self::assertEquals([
             'Header-1' => 'value-1',

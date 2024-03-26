418f5dc807f72626fb866c37fbccc2f64a59ffe5
forszaken <snjtolibjonov@gmail.com>
2024-03-26 12:14:28 +0500

    Test for helper

---
+++ tests/NetWeaver/Http/Message/TextResponseTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\NetWeaver\Http\Message;
+
+use Laminas\Diactoros\ResponseFactory;
+use PHPUnit\Framework\TestCase;
+
+use function Framework\Http\Message\textResponse;
+
+class TextResponseTest extends TestCase
+{
+    public function testDefault(): void
+    {
+        $origin = (new ResponseFactory())->createResponse();
+
+        $response = textResponse($origin, $content = 'Content');
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals('text/plain; charset=utf-8', $response->getHeaderLine('Content-Type'));
+        self::assertEquals($content, (string)$response->getBody());
+    }
+
+    public function testCode(): void
+    {
+        $origin = (new ResponseFactory())->createResponse();
+
+        $response = textResponse($origin, $content = 'Content with Code', 203);
+
+        self::assertEquals(203, $response->getStatusCode());
+        self::assertEquals('text/plain; charset=utf-8', $response->getHeaderLine('Content-Type'));
+        self::assertEquals($content, (string)$response->getBody());
+    }
+}

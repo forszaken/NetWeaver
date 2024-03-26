29bc9855831a5f277ed641b88f7719805733c0ca
forszaken <snjtolibjonov@gmail.com>
2024-03-26 10:39:28 +0500

    Update init front controller, fix text && globals

--- public/index.php
+++ public/index.php
@@ -2,53 +2,64 @@
 
 declare(strict_types=1);
 
-namespace Framework\Http;
+use NetWeaver\Http\Message\DiactorosResponseFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiStreamEmitter;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
-use NetWeaver\Http\Message\DiactorosStreamFactory;
-use NetWeaver\Http\Message\DiactorosUriFactory;
-use Laminas\Diactoros\ServerRequest;
-use Laminas\Diactoros\Stream;
-use Laminas\Diactoros\Uri;
+use function App\detectLang;
+use function NetWeaver\Http\createDiactorosServerRequestFromGlobals;
 
-/**
- * @param array<string, array|string>|null $query
- * @param array<string, array|string>|null $body
- * @param array<string, string>|null $server
- * @param resource|null $input
- */
-function createDiactorosServerRequestFromGlobals(
-    ?array $server = null,
-    ?array $query = null,
-    ?array $cookie = null,
-    ?array $body = null,
-    mixed $input = null
-): ServerRequest {
-    $server ??= $_SERVER;
+http_response_code(500);
 
-    $headers = [
-        'Content-Type' => [$server['CONTENT_TYPE']],
-        'Content-Length' => [$server['CONTENT_LENGTH']],
-    ];
+/** @psalm-suppress MissingFile */
+require __DIR__ . '/../vendor/autoload.php';
 
-    foreach ($server as $serverName => $serverValue) {
-        if (str_starts_with($serverName, 'HTTP_')) {
-            $name = ucwords(strtolower(str_replace('_', '-', substr($serverName, 5))), '-');
-            /** @var string[] $values */
-            $values = preg_split('#\s*,\s*#', $serverValue);
-            $headers[$name] = $values;
-        }
+### Page
+class Home
+{
+    private readonly DiactorosResponseFactory $factory;
+
+    public function __construct(DiactorosResponseFactory $factory)
+    {
+        $this->factory = $factory;
     }
 
-    return new ServerRequest(
-        serverParams: $server,
-        uri: (new DiactorosUriFactory())->createUri(
-            (!empty($server['HTTPS']) ? 'https' : 'http') . '://' . $server['HTTP_HOST'] . $server['REQUEST_URI']
-        ),
-        method: $server['REQUEST_METHOD'],
-        body: (new DiactorosStreamFactory())->createStreamFromResource($input ?: fopen('php://input', 'r')),
-        headers: $headers,
-        cookieParams: $cookie ?? $_COOKIE,
-        queryParams: $query ?? $_GET,
-        parsedBody: $body ?? ($_POST ?: null)
-    );
+    public function __invoke(ServerRequestInterface $request): ResponseInterface
+    {
+        $name = $request->getQueryParams()['name'] ?? 'Guest';
+
+        if (!is_string($name)) {
+            return $this->factory->createResponse(400);
+        }
+
+        $lang = detectLang($request, 'en');
+
+        $response = $this->factory->createResponse()
+            ->withHeader('Content-Type', 'text/plain; charset=utf-8');
+
+        $response->getBody()->write('Hello, ' . $name . '! Your lang is ' . $lang);
+
+        return $response;
+    }
 }
+
+### Grabbing
+$request = createDiactorosServerRequestFromGlobals();
+
+### Preprocessing
+if (str_starts_with($request->getHeaderLine('Content-Type'), 'application/x-www-form-urlencoded')) {
+    parse_str((string)$request->getBody(), $data);
+    $request = $request->withParsedBody($data);
+}
+
+### Running
+$home = new Home(new DiactorosResponseFactory());
+$response = $home($request);
+
+### Postprocessing
+$response = $response->withHeader('X-Frame-Options', 'DENY');
+
+### Sending
+$emitter = new SapiStreamEmitter();
+$emitter->emit($response);
